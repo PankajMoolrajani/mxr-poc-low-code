cd1bd10ef6de138f11415fef057b0ce81c9f36c8
PankajMoolrajani <pmoolrajani@gmail.com>
2020-10-06 14:28:46 +0000

    config based routing

--- poc-low-code/src/App.js
+++ poc-low-code/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { observer } from "mobx-react";
 import Box from "@material-ui/core/Box";
-import ToDoApp from "./apps/ToDoApp";
+import ToDoApp from "./apps/ToDoApp.json";
+import CrmApp from "./apps/CrmApp.json"
 import tasks from "./data/tasks.json";
 import Container from "./components/Container"
 
@@ -11,7 +13,12 @@ class App extends Component {
     let data = tasks.tasks;
     return (
       <Box style={{ width: "100%", textAlign: "center" }}>
-        <Container uiConfig={ToDoApp} data={data} />
+        <Router>
+          <Switch>
+            <Route path="/crm" component={() => <Container uiConfig={CrmApp} data={data} />} />
+            <Route path="/todo" component={() => <Container uiConfig={ToDoApp} data={data} />} />
+          </Switch>
+        </Router>        
       </Box>
     );
   }
